3ae431dd6624a9d33fe92703b3e699501cc890cd
Roger <williroger@usp.br>
2021-01-04 15:55:51 -0300

    teste svg
    testando nvoas formas

--- app.js
+++
@@ -1,9 +0,0 @@
-function bgMuda (){
-    if(this.scrollY > this.innerHeight/2) {
-    document.body.classList.add('bg-ativado');
-}else{
-    document.body.classList.remove('bg-ativado');
-}
-}
-
-window.addEventListener('scroll', bgMuda);
